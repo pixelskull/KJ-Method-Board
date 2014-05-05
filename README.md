a1868a48d41522991c8441694804bf6d1dd6c633
pascal <pascal@alufolie.org>
2014-05-05 17:12:48 +0200

    basic implementation of Lazy Susan Element, fullscreen-mode activ

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ SGMCI-KJ-Method-Kivy
 ====================
 
 KJ-Method Board in Kivy/Python
+
 for Multiple VKeyboards use the "-c kivy:keyboard_mode:multi" argument 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App 
+from kivy.config import Config
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
@@ -9,40 +10,55 @@ from kivy.uix.button import Button
 from kivy.uix.scatter import Scatter
 from kivy.uix.vkeyboard import VKeyboard
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty
 
+import math
 
-class NumericTextInput(TextInput):
-    def on_focus(self, instance, value, *largs):
-        win = self.get_root_window()
+# Config.set('graphics', 'width', '1650')
+# Config.set('graphics', 'height', '1050')
+# Config.write()
 
-        if win:
-            win.release_all_keyboards()
-            win._keyboards = {}
+class LazySusan(Widget): 
+    angle = NumericProperty(0) 
+    center_position = NumericProperty(0) 
 
-            if value: #User focus; use special keyboard
-                win.set_vkeyboard_class(NumericVKeyboard)
-                print "NumericVKeyboard:", win._vkeyboard_cls, VKeyboard.layout_path
-            else: #User defocus; switch back to standard keyboard
-                win.set_vkeyboard_class(VKeyboard)
-                print ":", win._vkeyboard_cls, VKeyboard.layout_path
-        return TextInput.on_focus(self, instance, value, *largs)
+    def __init__(self, **kwargs):
+        super(LazySusan,self).__init__(**kwargs)
+        # center_position = Window.center
+
+    def on_touch_down(self, touch):
+        y = (touch.y - self.center[1])
+        x = (touch.x - self.center[0])
+        calc = math.degrees(math.atan2(y, x))
+        self.prev_angle = calc if calc > 0 else 360+calc
+        self.tmp = self.angle
+
+    def on_touch_move(self, touch):
+        y = (touch.y - self.center[1])
+        x = (touch.x - self.center[0])
+        calc = math.degrees(math.atan2(y, x))
+        new_angle = calc if calc > 0 else 360+calc
+
+        self.angle = self.tmp + (new_angle-self.prev_angle)%360
+
 
 class KJMethod(FloatLayout):
-	def open_input(self):
-		s = Scatter()
-		inpt = TextInput(size_hint=(None, None), size=(100, 50), pos=(self.parent.width/2, self.parent.height/2))
-		s.add_widget(inpt)
-		self.add_widget(s)
+    print Window.center
 
-	def __init__(self, **kwargs):
-		super(KJMethod, self).__init__(**kwargs)
+	# def open_input(self):
+	# 	s = Scatter()
+	# 	inpt = TextInput(size_hint=(None, None), size=(100, 50), pos=(self.parent.width/2, self.parent.height/2))
+	# 	s.add_widget(inpt)
+	# 	self.add_widget(s)
+
+	# def __init__(self, **kwargs):
+	# 	super(KJMethod, self).__init__(**kwargs)
 
 class KJMethodApp(App):
 	def build(self):
 		scene = KJMethod()
 		return scene
 
-
+Window.fullscreen = True
 if __name__ == '__main__':
  	KJMethodApp().run()
