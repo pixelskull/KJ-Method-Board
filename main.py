68b47d0337b2fd85c5e611ad9945feaf78bcb1c6
pascal <pascal@alufolie.org>
2014-05-14 15:48:02 +0200

    minor bugfixes, menu opens with two finger tap (after 0.5 sec) and closes after 3 sec, EditableLabel implemented

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ from kivy.uix.button import Button
 from kivy.uix.scatter import Scatter
 from kivy.uix.vkeyboard import VKeyboard
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
-from kivy.properties import ObjectProperty, NumericProperty, ListProperty
+from kivy.properties import ObjectProperty, NumericProperty, ListProperty, BooleanProperty
 from functools import partial
 from kivy.base import EventLoop
 EventLoop.ensure_window()
@@ -28,26 +29,24 @@ import kwad
 
 class Menu(Widget): 
 
-    def create_clock(self, widget, touch, *args):
-        # if self.collide_point(touch.pos[0], touch.pos[1]) is  True:
-        if touch.is_double_tap:
-            callback = partial(self.open_menu, touch)
-            Clock.schedule_once(callback, 1)
-            touch.ud['menu_event'] = callback 
-        else: 
-            touch.ud['menu_event'] = None          
+    touches = []
+    app = None;
 
-    def delete_clock(self, widget, touch, *args):
-        if touch.ud['menu_event'] is not None:
-            Clock.unschedule(touch.ud['menu_event'])
-        
-    # def on_touch_move(self, touch): 
-    #     print("move move move")
-    #     Clock.unschedule(touch.ud['menu_event'])
+    def save_touch_down(self, instance, touch):
+        touch.ud['menu_event'] = None
+        if len(self.touches) >= 1:
+            for t in self.touches:
+                if t.distance(touch) <= 50:
+                    callback = partial(self.open_menu, touch)
+                    Clock.schedule_once(callback, 0.5)
+                    touch.ud['menu_event'] = callback
+        self.touches.append(touch)
 
-    # def on_touch_up(self, touch): 
-    #     print("up up up")
-    #     Clock.unschedule(touch.ud['menu_event'])
+    def remove_touch_down(self, instance, touch):
+        if touch in self.touches:
+            if touch.ud['menu_event'] is not None:
+                Clock.unschedule(touch.ud['menu_event'])
+            self.touches.remove(touch)
 
     def open_menu(self, touch, *args): 
         menu = BoxLayout(
@@ -59,26 +58,32 @@ class Menu(Widget):
         close = Button(text='close')
         close.bind(on_release=partial(self.close_menu, menu))
         menu.add_widget(close)
-        self.add_widget(menu)
+        self.parent.add_widget(menu)
+        callback = partial(self.close_menu, menu)
+        Clock.schedule_once(callback, 2.5)
 
     def close_menu(self, widget, *args):
-        self.remove_widget(widget)
+        self.parent.remove_widget(widget)
 
     def change_view(self, widget, *args):
-        self.root.sm.current = 'sort'
+        self.app.sm.current = 'sort'
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs): #, parent
         super(Menu, self).__init__(**kwargs)
-        self.root = KJMethodApp()
-        Window.bind(on_touch_down=self.create_clock)
-        Window.bind(on_touch_up=self.delete_clock)
+        self.app = KJMethodApp.get_running_app()
+        self.bind(on_touch_down=self.save_touch_down)
+        self.bind(on_touch_up=self.remove_touch_down)
+        self.bind(on_touch_move=self.remove_touch_down)
+
 
 class LazySusan(Widget): 
     lazy_angle = NumericProperty(0) 
     tmp = None
 
     def on_touch_down(self, touch):
+        
         if Widget(pos=self.pos, size=self.size).collide_point(touch.pos[0], touch.pos[1]):
+            print('lazy touch down')
             y = (touch.y - self.center[1])
             x = (touch.x - self.center[0])
             calc = math.degrees(math.atan2(y, x))
@@ -86,57 +91,93 @@ class LazySusan(Widget):
             self.tmp = self.lazy_angle
 
     def on_touch_move(self, touch):
+        
         if Widget(pos=self.pos, size=self.size).collide_point(touch.pos[0], touch.pos[1]) and self.tmp is not None:
+            print('lazy touch move')
             y = (touch.y - self.center[1])
             x = (touch.x - self.center[0])
             calc = math.degrees(math.atan2(y, x))
             new_angle = calc if calc > 0 else 360+calc
             self.lazy_angle = self.tmp + (new_angle-self.prev_angle)%360
 
+    def __init__(self, **kwargs):
+        super(LazySusan, self).__init__(**kwargs)
+
+
+class EditableLabel(Label):
+
+    edit = BooleanProperty(False)
+    move = BooleanProperty(False)
+
+    textinput = ObjectProperty(None, allownone=True)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos) and not self.edit:
+            self.edit = True
+        return super(EditableLabel, self).on_touch_down(touch)
+
+    def on_touch_move(self, touch): 
+        self.textinput.focus = False 
+
+    def on_touch_up(self, touch):
+        self.edit = False 
+
+    def on_edit(self, instance, value):
+        if not value:
+            if self.textinput:
+                self.remove_widget(self.textinput)
+            return
+        if not self.textinput:
+            self.textinput = t = TextInput(
+                text=self.text, size_hint=(None, None),
+                font_size=self.font_size, font_name=self.font_name,
+                pos=self.pos, size=self.size, multiline=False, keyboard_mode='managed')
+            self.bind(pos=t.setter('pos'), size=t.setter('size'))
+            self.add_widget(self.textinput)
+            self.textinput.bind(on_text_validate=self.on_text_validate, focus=self.on_text_focus)
+
+        else:
+            self.add_widget(self.textinput) 
+
+
+    def on_text_validate(self, instance):
+        self.text = instance.text
+        self.edit = False
+
+    def on_text_focus(self, instance, focus):
+        if focus is False or self.edit is False:
+            self.text = instance.text
+            self.edit = False
+            self.textinput.hide_keyboard()  
+        else:
+            self.textinput.show_keyboard()
+
+    def __init__(self, **kwargs):
+        super(EditableLabel, self).__init__(**kwargs)
+        self.show_area(color='green', alpha=0.5, group=None)
+
 
 class KJMethod(FloatLayout):
-    # lazy_susan = ObjectProperty(LazySusan())
+
     def add_label(self, widget, *args): 
         s = Scatter(size_hint=(None,None), size=(100,50), pos=(self.parent.width/2, self.parent.height/2))
-        inpt = TextInput(size_hint=(None, None), size=(100, 50), on_focus=self.request_vkeyboard)
+        inpt = EditableLabel(text="hallo", size_hint=(None, None), size=(100, 50), keyboard_mode='managed')
+        # inpt.bind(on_touch_up=show_keyboard())
         # KeyboardListener().setCallback(self.key_up)
         s.add_widget(inpt)
-        print self
-        self.parent.add_widget(s)
+        self.add_widget(s)
 
-    def request_vkeyboard(self):
-        keyboard = Window.request_keyboard(self._keyboard_close, self)
-        print("keyboard")
-        if keyboard.widget:
-            print("keyboard")
-            vkeyboard = self._keyboard.widget
-            vkeyboard.layout = './qwertz.json'
+    def __init__(self, **kwargs):
+        super(KJMethod, self).__init__(**kwargs)
 
-    def _keyboard_close(self, *args):
-        """ The active keyboard is being closed. """
-        if self._keyboard:
-            self._keyboard.unbind(on_key_down=self.key_down)
-            self._keyboard.unbind(on_key_up=self.key_up)
-            self._keyboard = None
-
-    def key_up(self, keyboard, keycode, text, modifiers):
-        self.label.text = self.label.text + keycode[1]
-
-    # def __init__(self, **kwargs):
-    #     super(KJMethod, self).__init__(**kwargs)
-    #     self._keyboard = None
-
-	# def open_input(self):
-	# 	s = Scatter()
-	# 	inpt = TextInput(size_hint=(None, None), size=(100, 50), pos=(self.parent.width/2, self.parent.height/2))
-	# 	s.add_widget(inpt)
-	# 	self.add_widget(s)
 
 class KJSort(FloatLayout): 
     pass
 
+
 class KJMethodScreen(Screen):
     pass
+
 
 class KJSortScreen(Screen):
     pass
@@ -149,10 +190,7 @@ class KJMethodApp(App):
         self.sm.add_widget(KJMethodScreen(name="method"))
         self.sm.add_widget(KJSortScreen(name="sort"))
         return self.sm
- 
-    # def goto_screen(self, screen_name):
-    #     self.root.clear_widgets()
-    #     self.root.add_widget(self.screens[screen_name])
+
 
 Config.set('graphics', 'width', '1280')
 Config.set('graphics', 'height', '800')
