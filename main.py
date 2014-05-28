bb44282eaa70f326870e3641b9c6c02b1100c07c
pixelskull <pascal@alufolie.org>
2014-05-28 14:18:37 +0200

    sync with json files, add label to lazySusan, central Singleton pattern for new Card class

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ EventLoop.ensure_window()
 from kivy.logger import Logger
 import math
 import os
+import os.path
+import json
 
 import kwad
 
@@ -72,7 +74,8 @@ class Menu(Widget):
 
     # method for changeing view 
     def change_view(self, widget, *args):
-        self.app.sm.current = 'sort'
+        # self.app.sm.current = 'sort'
+        self.app.sm.switch_to(KJSortScreen(name="sort"))
 
     # initialisation method
     def __init__(self, **kwargs): #, parent
@@ -87,6 +90,12 @@ class Menu(Widget):
 class LazySusan(Widget): 
     lazy_angle = NumericProperty(0) 
     tmp = None
+    label1 = ObjectProperty(None)
+    label2 = ObjectProperty(None)
+    label3 = ObjectProperty(None)
+    label4 = ObjectProperty(None)
+    label5 = ObjectProperty(None)
+
 
     # called when finger touch is detected 
     def on_touch_down(self, touch):
@@ -105,6 +114,31 @@ class LazySusan(Widget):
             calc = math.degrees(math.atan2(y, x))
             new_angle = calc if calc > 0 else 360+calc
             self.lazy_angle = self.tmp + (new_angle-self.prev_angle)%360
+
+    def sync_entrys_in_lazy_susan(self, widget, *args):
+        lazy_json = open('lazy.json', 'r')
+        data = []
+        try:
+            data = json.load(lazy_json)
+        except Exception, e:
+            pass
+        if len(data) >= 1:
+            self.label1.text = data[0]
+        if len(data) >= 2:
+            self.label2.text = data[1]
+        if len(data) >= 3:
+            self.label3.text = data[2]
+        if len(data) >= 4:
+            self.label4.text = data[3]
+        if len(data) >= 5:
+            self.label5.text = data[4]
+            
+
+    # init method 
+    def __init__(self, **kwargs):
+        super(LazySusan, self).__init__(**kwargs) 
+        update = self.sync_entrys_in_lazy_susan
+        Clock.schedule_interval(update, 0.1)
 
 
 # Class for label that is editable 
@@ -143,39 +177,92 @@ class EditableLabel(Label):
 
     # called when text is entered 
     def on_text_validate(self, instance):
-        self.text = instance.text
-        self.edit = False
+        if instance.text is not '':
+            self.text = instance.text
+            self.edit = False
+        else: 
+            instance.text = 'touch me'
 
     # called when label has focus (touch on label)
     def on_text_focus(self, instance, focus):
+        if self.textinput.text == 'touch me':
+            self.textinput.text = ""
         if focus is False or self.edit is False:
-            self.text = instance.text
+            if self.textinput.text == '':
+                self.textinput.text = 'touch me'
+            else: 
+                self.text = instance.text
             self.edit = False
             self.textinput.hide_keyboard() 
-            Singelton(Card).add_card(self.text) 
+            #Singelton(Card).add_card(self.text) 
         else:
             self.textinput.show_keyboard()
+            #Singelton(Card).remove_card(self.text)
+
+    def __init__(self, **kwargs):
+        super(EditableLabel, self).__init__(**kwargs)
 
 
 # dummy implementation for saving Cards 
 class Card():
     _instance = None
-    cards = []
+    cards = {'default':[]}
+    cards_changed = False
+    file_exist = False
+    file_changed = False
+    new_entrys_from_file = []
 
     # add a card to cards-list 
     def add_card(self, label): 
-        if label not in self.cards:
-            self.cards.append(label)
+        if label not in self.cards['default']:
+            if label is not '':
+                self.cards['default'].append(label)
+                self.cards_changed = True
         print self.cards
 
     # removes a card to cards-list 
     def remove_card(self, label):
-        if label in self.cards:  
-            self.cards.remove(label)
+        if label in self.cards['default']:
+            if label is not '':  
+                self.cards['default'].remove(label)
+                self.cards_changed = True
         print self.cards
 
+    # syncronising Cards with Json files 
+    def sync_cards_withFiles(self, widget, *args):
+        with open('lazy.json', 'w') as outfile: 
+            json.dump(self.cards['default'][-10:], outfile);
+        if self.cards_changed is True:
+            #print('syncronising')             
+            main_json = open('main.json', 'r+')
+            main_data = None
+            try:
+                main_data = json.load(main_json)
+            except Exception, e:
+                pass
+            #print('maindata: ')
+            #print main_data
+            with open('main.json', 'w') as outfile:
+                json.dump(self.cards, outfile)
+            self.cards_changed = False 
+        main_json = open('main.json', 'r+')
+        try:
+            main_data = json.load(main_json)
+            for item in main_data['default']:
+                if item not in self.cards['default']:
+                    self.add_card(item)
+                    #print self.cards
+        except Exception, e:
+            pass
+
+    # init method 
+    def __init__(self, **kwargs): 
+        update = self.sync_cards_withFiles
+        Clock.schedule_interval(update, 1.0)
+
+
 # method vor calling a single instance of a Class 
-def Singelton(instanceClass): 
+def Singleton(instanceClass): 
     if not instanceClass._instance: 
         instanceClass._instance = instanceClass()
     return instanceClass._instance
@@ -189,7 +276,7 @@ class KJMethod(FloatLayout):
     def add_label(self, widget, *args): 
         s = Scatter(size_hint=(None,None), size=(100,50), pos=widget.pos)
         #(self.parent.width/2, self.parent.height/2))
-        inpt = EditableLabel(text="hallo", size_hint=(None, None), size=(100, 50), keyboard_mode='managed')
+        inpt = EditableLabel(text='touch me', size_hint=(None, None), size=(100, 50), keyboard_mode='managed')
         # inpt.bind(on_touch_up=show_keyboard())
         # KeyboardListener().setCallback(self.key_up)
         s.add_widget(inpt)
@@ -200,26 +287,57 @@ class KJMethod(FloatLayout):
     # callback called for checking delete option 
     def update_scatter(self, s, dt):
         if (s.pos[1] < 0) or (s.top > Window.height) or (s.pos[0] < 0) or (s.right > Window.width):
+            # s.children[0].color(1,0,0,1)
             self.delete_scatter = True
             callback = partial(self.remove_widget_callback, s)
             Clock.schedule_once(callback, 3.0)
+        for child in self.children:
+            if (type(child) is LazySusan):
+                if child.collide_widget(s):
+                    self.delete_scatter = True
+                    # print s.children[0]
+                    # s.children[0].color(0,1,0,1)
+                    Singleton(Card).add_card(s.children[0].text)
+                    callback = partial(self.remove_widget_callback, s)
+                    Clock.schedule_once(callback, 1.5)
+                # else: 
+                #     s.children[0].show_area(color='green', alpha=0.0, group=None)
+                #     self.delete_scatter = False
+                #     Singleton(Card).remove_card(s.children[0].text)
 
     # removes (delete) an scatter 
     def remove_widget_callback(self, widget, *args):
         if self.delete_scatter is True:
-            Singelton(Card).remove_card(widget.children[0].text)
+            # print widget.children[0]
+            Singleton(Card).remove_card(widget.children[0].text)
             self.remove_widget(widget)
             self.delete_scatter = False
+
+    def __init__(self, **kwargs): 
+        super(KJMethod, self).__init__(**kwargs)
+        try:
+            os.remove('./main.json')
+            open('main.json', 'w')
+        except Exception, e:
+            pass
+        try:
+            os.remove('./lazy.json')
+            open('lazy.json', 'w')
+        except Exception, e:
+            pass
+        
 
 
 # implementation Class for the second Screen
 class KJSort(FloatLayout): 
-    labelset = False
-
+    labelset = True
+    _instance = None
     # method for adding all cards to second screen  (at the moment only one label ist added)
     def add_labels(self, widget, **args): 
         if self.labelset is False:
+            #print Singelton(Card).cards
             for label in Singelton(Card).cards:
+                #print label
                 s = Scatter(size_hint=(None,None), size=(100,50))
                 inpt = Label(text=label, size_hint=(None,None), size=(100,50), keyboard_mode='managed')
                 s.add_widget(inpt)
@@ -229,8 +347,9 @@ class KJSort(FloatLayout):
     # init method
     def __init__(self, **kwargs): 
         super(KJSort, self).__init__(**kwargs)
+        # self.bind(on_enter=self.add_labels)
         update = self.add_labels
-        Clock.schedule_interval(update, 1.0/60.0)
+        Clock.schedule_interval(update, 0.5)
 
 
 # Dummy Class for the first Screen (definition in .kv file)
@@ -239,16 +358,19 @@ class KJMethodScreen(Screen):
 
 # Dummy Class for the second Screen (definition in .kv file)
 class KJSortScreen(Screen):
-    pass
+    # pass
+    def on_enter(self):
+        self.children[0].labelset = False
+
 
 #basic Class, root for this Application
-class KJMethodApp(App):
+class KJMethodApp(App, ScreenManager):
     sm = ObjectProperty(ScreenManager(transition=SlideTransition()))
 
     # default build method
     def build(self):
         self.sm.add_widget(KJMethodScreen(name="method"))
-        self.sm.add_widget(KJSortScreen(name="sort"))
+        # self.sm.add_widget(KJSortScreen(name="sort"))
         return self.sm
 
 #resolution settings
